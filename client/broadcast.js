c7f11b7747c611bde357e8498b84f617b9331d68
Dana Tran <dana.l.tran@gmail.com>
2016-10-04 15:46:30 -0700

    stored events, stored broadcasters to events, removed broadcasters from events, delete event if no broadcasters

--- client/broadcast.js
+++ client/broadcast.js
@@ -1,5 +1,19 @@
-var socket = io();
-socket.on('connect', function(){
+const socket = io();
+socket.on('connect', () => {
   console.log('broadcaster socket connected', socket.id);
+  
 });
 
+sendEventTag = () => {
+  let eventTag = $('#eventTag').val();
+  $('#startButton').prop('disabled', true);
+  $('#stopButton').prop('disabled', false);
+  socket.emit('sendEventTag', eventTag);
+};
+
+stopStream = () => {
+  let eventTag = $('#eventTag').val();
+  $('#startButton').prop('disabled', false);
+  $('#stopButton').prop('disabled', true);
+  socket.emit('removeBroadcaster', eventTag);
+};
