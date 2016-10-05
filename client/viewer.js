13d809ffabc8587a6c4d21c187af14a31d2b121e
dianacheung <diana.cheung.2013@gmail.com>
2016-10-05 15:49:03 -0700

    able to do 1 peer connection between broadcaster and viewer

--- client/viewer.js
+++ client/viewer.js
@@ -14,17 +14,77 @@ $('#eventName').html(eventTag);
 //   video.play()
 // });
 
-
+var viewerPeer;
 const socket = io();
 socket.on('connect', () => {
   console.log('viewer socket connected', socket.id);
-  socket.emit('getBroadcastURL', eventTag);
 
-  socket.on('sendBroadcastURL', (broadcastURL) => {
-    var video = document.createElement('video');
-    console.log('BROADCASTURL:', broadcastURL);
-    video.src = broadcastURL;
-    document.body.appendChild(video);
-    video.play();
+  // view wants to initiate contact with broadcaster
+  socket.emit('initiateView', eventTag);
+
+  socket.on('signal', (peerObj) => {
+    console.log('viewer receiving signal', peerObj);
+
+    // instantiate viewerPeer
+    if(viewerPeer === undefined) {
+      console.log('inside viewerPeer instantiate', peerObj);
+      var broadcasterId = '/#' + peerObj.peerId;
+      viewerPeer = new SimplePeer({initiator: false});
+      viewerPeer.signal(peerObj.data);
+
+      viewerPeer.on('signal', (data) => {
+        console.log('viewerPeer on signal', data);
+
+        // emit message to server to pass on to broadcaster
+        socket.emit('signal', broadcasterId, {peerId: socket.id, data: data});
+      });
+
+      viewerPeer.on('error', function(e) {
+        console.log('inside viewer, Error sending connection:', e);
+      });
+
+      viewerPeer.on('connect', function () {
+        console.log('viewerPeer connect');
+        //broadcasterPeer.send('hey viewer, how is it going?')
+      });
+
+      viewerPeer.on('stream', function (stream) {
+        console.log('got a stream from broadcaster');
+        // got remote video stream, now let's show it in a video tag
+        var video = $('#broadcast1')[0];
+        video.src = window.URL.createObjectURL(stream)
+        video.play()
+      })
+
+      // viewerPeer.on('data', (msg) => {
+      //   console.log('got a message from broadcaster: ' + msg);
+      // });
+    }
+    // var viewerPeer = new SimplePeer({initiator: false});
+
+    // viewerPeer.on('signal', (data) => {
+    //   console.log('viewerPeer on signal');
+
+    //   // emit message to server to pass on to broadcaster
+    //   socket.emit('signal', peerObj.userId, {userId: socket.id, data: data});
+    // });
+
+    // viewerPeer.on('message', (msg) => {
+    //   console.log('got a message from broadcaster: ' + msg);
+    // });
+
+    //viewerPeer.signal(peerObj.data);
+
+    
   });
+
+  // socket.emit('getBroadcastURL', eventTag);
+
+  // socket.on('sendBroadcastURL', (broadcastURL) => {
+  //   var video = document.createElement('video');
+  //   console.log('BROADCASTURL:', broadcastURL);
+  //   video.src = broadcastURL;
+  //   document.body.appendChild(video);
+  //   video.play();
+  // });
 });
