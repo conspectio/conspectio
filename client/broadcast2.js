e36dcab8cf291b8bbe3ca420fcce7fee5564a734
Simon Situ <ssitu001@gmail.com>
2016-10-07 21:56:33 -0700

    add public turn servers

--- client/broadcast2.js
+++ client/broadcast2.js
@@ -18,6 +18,21 @@ class ConspectioBroadcaster {
       'iceServers': [
         {
           'url': 'stun:stun.l.google.com:19302'
+        },
+        {
+          url: 'turn:numb.viagenie.ca',
+          credential: 'muazkh',
+          username: 'webrtc@live.com'
+        },
+        {
+          url: 'turn:192.158.29.39:3478?transport=udp',
+          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+          username: '28224511:1379330808'
+        },
+        {
+          url: 'turn:192.158.29.39:3478?transport=tcp',
+          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+          username: '28224511:1379330808'
         }
       ]
     });
--- client/viewer2.js
+++ client/viewer2.js
@@ -15,6 +15,21 @@ class ConspectioViewer {
       'iceServers': [
         {
           'url': 'stun:stun.l.google.com:19302'
+        },
+        {
+          url: 'turn:numb.viagenie.ca',
+          credential: 'muazkh',
+          username: 'webrtc@live.com'
+        },
+        {
+          url: 'turn:192.158.29.39:3478?transport=udp',
+          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+          username: '28224511:1379330808'
+        },
+        {
+          url: 'turn:192.158.29.39:3478?transport=tcp',
+          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+          username: '28224511:1379330808'
         }
       ]
     });
@@ -116,8 +131,10 @@ socket.on('connect', () => {
       newPC.receiveOffer(message.offer);
       newPC.createAnswerWrapper();
     } else if (message.type === 'candidate') {
-      var currentPC = connections[fromId];
-      currentPC.addCandidate(message.candidate);
+      if (fromId){
+        var currentPC = connections[fromId];
+        currentPC.addCandidate(message.candidate);
+      }
     }
   });
 
